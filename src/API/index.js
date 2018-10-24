45f5fb2577b3e31732b88008516369acc950a4ec
Sabrina Tolle <sabrinatolle@outlook.com>
2018-10-24 19:15:24 -0400

    add foursquare credentials

---
+++ src/API/index.js
@@ -0,0 +1,12 @@
+class Helper {
+    static baseURL(){
+        return "https://api.foursquare.com/v2"
+    }
+
+    static auth(){
+        const keys = {
+            client_id: "VJDFLA50R3APBBVQMBZQ4EZKIG5LERLAYZF1EC5CYHK1AZ2P",
+            client_secret: "RSLIAHUYURS32RIPTKRR30QSU4F5SYKAYUX042PKYJPSB4U2"
+        }
+    }
+}
