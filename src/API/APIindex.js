f0a0448a05e6484c495f0b8d64c2bc955e2408e9
Sabrina Tolle <sabrinatolle@outlook.com>
2018-11-02 12:34:17 -0400

    import SquareAPI

--- src/API/APIindex.js
+++ src/API/APIindex.js
@@ -6,7 +6,50 @@ class Helper {
     static auth(){
         const keys = {
             client_id: "VJDFLA50R3APBBVQMBZQ4EZKIG5LERLAYZF1EC5CYHK1AZ2P",
-            client_secret: "RSLIAHUYURS32RIPTKRR30QSU4F5SYKAYUX042PKYJPSB4U2"
+            client_secret: "RSLIAHUYURS32RIPTKRR30QSU4F5SYKAYUX042PKYJPSB4U2",
+            v: "20181102"
+        };
+        return Object.keys(keys)
+        .map(key => `${key}=${keys[key]}`)
+        .join("&");
+    }
+    static urlBuider(urlPrams) {
+        tf (!urlPrans) {
+            return "";
         }
+        return Object.keys(urlPrams)
+        .map(key => `${key}=${urlPrams[key]}`)
+        .join("&");
+    }
+    static headers() {
+        return {
+            Accept: "application/json"
+        };
+    }
+    static simpleFetch(endPoint, method, urlPrams) {
+        let requestData = {
+            method,
+            headers: Helper.headers()
+        };
+        return fetch (
+            `${Helper.baseURL()}${endPoint}?${Helper.auth()}&${Helper.urlBuilder(
+                urlPrams
+            )}`,
+            requestData
+        ).then(res => res.json());
+
+    }
+
+} 
+export default class SquareAPI {
+    static search(urlPrams) {
+        return Helper.simpleFetch("/venues/search", "GET", urlPrams);
+    }
+    static getVenueDetails(VENUE_ID){
+        return Helper.simpleFetch(`/venues/${VENUE_ID}`,"GET");
+
+    }
+    static getVenuePhotos(VENUE_ID) {
+        return Helper.simpleFetch(`/venues/${VENUE_ID}/photos`, "GET");
     }
 }
--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 
 import './App.css';
 import Map from "./component/map.js";
+import SquareAPI from "./API/";
 
 class App extends Component {
+  componentDidMount() {
+    SquareAPI.search({
+      near: "Columbia,SC",
+      query: "tacos",
+      limit: 10
+    }).then(results => console.log(results));
+  }
   render() {
     return (
       <div className="App">
