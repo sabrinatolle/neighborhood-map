a8bebdb51be89adc617198891c636b2cdb5f872a
Sabrina Tolle <sabrinatolle@outlook.com>
2018-11-07 14:29:18 -0500

    add search input

--- src/component/sidebar.js
+++ src/component/sidebar.js
@@ -5,6 +5,7 @@ export default class SideBar extends Component {
     render() {
         return (
         <div className="sideBar">
+        <input type={"search"} id={"search"} placeholder={"Filter Venues"} />
          <VenueList />
         </div>)
     }
