ab8f489d72de5291df50142dc99230fbeb9ce16d
Sabrina Tolle <sabrinatolle@outlook.com>
2018-11-07 13:49:50 -0500

    create component file venueList

---
+++ src/component/sidebar.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+
+export default class sideBar extends Component {
+    render() {
+        return (<div className="sideBar">
+        
+        </div>)
+    }
+}
---
+++ src/component/venueList.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+export default class venueList extends Component {
+    render(){
+        return (
+            <ol className ="venueList">
+            </ol>
+        )
+    }
+}
