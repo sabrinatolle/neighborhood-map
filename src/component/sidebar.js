9b5b74646de08eef3a8d1f8da94181e111eb4247
Sabrina Tolle <sabrinatolle@outlook.com>
2018-11-07 13:55:31 -0500

    import venueList

--- src/component/sidebar.js
+++ src/component/sidebar.js
@@ -3,7 +3,7 @@ import React, {Component} from "react";
 export default class sideBar extends Component {
     render() {
         return (<div className="sideBar">
-        
+         <venueList />
         </div>)
     }
 }
