d935e9f8c791de2f272edf68b63a5546eff9b33b
Sabrina Tolle <sabrinatolle@outlook.com>
2018-11-07 13:42:13 -0500

    create new sidebar component

--- src/App.js
+++ src/App.js
@@ -12,7 +12,10 @@ class App extends Component {
       venues: [],
       markers: [],
       center: [],
-      zoom: 12
+      zoom: 12,
+      updateSuperState: obj => {
+        this.setState(obj);
+      }
       
     }; 
     
@@ -22,6 +25,7 @@ class App extends Component {
     const markers = this.state.markers.map(marker => {
       //allows only one marker to be open at a time
       marker.isOpen = false;
+      //marker.clickOnMarker = false;
       return marker;
     });
     this.setState({markers: Object.assign(this.state.markers, markers)});
@@ -30,16 +34,35 @@ class App extends Component {
   handleMarkerClick = marker => {
     this.closeAllMarkers();
     marker.isOpen = true;
-    this.setState({markers: Object.assign(this.state.markers,marker)})
-    SquareAPI.getVenueDetails(marker.id).then(res => console.log(res));
+    this.setState({markers: Object.assign(this.state.markers,marker)});
+    
+    const venue = this.state.venues.find(venue => venue.id === marker.id);
+   //gets info from FourSquare about each venue for infoWindow
+    SquareAPI.getVenueDetails(marker.id)
+    .then(res => {
+      const newVenue = Object.assign(venue, res.response.venue);
+      this.setState({venues: Object.assign(this.state.venues, newVenue)});
+      
+    })
+    .catch(error => {
+      this.setState({error})
+      console.log(this.state.error)
+    })
   };
-  
+
+ handleListItemClick = venue => {
+   const marker = this.state.markers.find(marker => marker.id === venue.id);
+   this.handleMarkerClick(marker);
+  };
+
   componentDidMount() {
     SquareAPI.search({
       near: "Columbia, SC",
-      query: "Mexican",
-      limit: 20
-    }).then(results => {
+      query: "Chinese",
+      limit: 10
+    })
+    
+    .then(results => {
       // deconstruct response
       const { venues } = results.response;
       const { center } = results.response.geocode.feature.geometry;
@@ -47,23 +70,29 @@ class App extends Component {
         return {
           lat: venue.location.lat,
           lng: venue.location.lng,
+          title: venue.name,
           isOpen:false,
           isVisible:true,
-          id: venue.id
-        }
+          id: venue.id,
+          name:venue.name,
+          address: venue.location.address
+        };
         
-      })
+      });
       this.setState({venues, center, markers});
       console.log(results)
       
     });
   }
+
+    
   render() {
     return (
       <div className="App">
-        <Map {...this.state}
-        handleMarkerClick={this.handleMarkerClick}/>
-        </div>
+         <Map {...this.state} handleMarkerClick={this.handleMarkerClick} />
+          </div>
+        
+      
     );
   }
 }
--- src/component/map.js
+++ src/component/map.js
@@ -19,36 +19,56 @@ const MyMapComponent = withScriptjs(
         center={props.center}
         defaultCenter={{ lat: 41.881832, lng: -87.623177 }}
         >
-        {/* shows the markers of results on map*/}
+        
          {props.markers && 
          props.markers
          .filter(marker => marker.isVisible)
-         .map((marker,idx)=>(
-        <Marker key={idx} position={{ lat: marker.lat, lng: marker.lng}} onclick={() => props.handleMarkerClick(marker)}>
-            {marker.isOpen && (
-            <InfoWindow>
-                <p>Hello</p>
-            </InfoWindow>
+         .map((marker,idx)=> {
+             const venueInfo = props.venues.find(venue => (venue.id===marker.id))
+        return (
+             <Marker 
+        key={idx} 
+        position={{ lat: marker.lat, lng: marker.lng}} 
+        onclick={() => props.handleMarkerClick(marker)}
+        >
+            {marker.isOpen && 
+            venueInfo.bestPhoto && (
+           //grabs photo and venue name 
+        <InfoWindow>
+            <React.Fragment> 
+                    <img 
+                    src={`${venueInfo.bestPhoto.prefix}200x200${
+                        venueInfo.bestPhoto.suffix
+                        }`}
+                        alt={`${venueInfo.name}`}
+                        />
+                
+            </React.Fragment>
+        </InfoWindow>
             )}
             </Marker>
 
-         ))}
+        );
+                    })}
         </GoogleMap>
         
     ))
+    
 
 
 export default class Map extends Component {
     render() {
-        return (<MyMapComponent
+        return (
+        <MyMapComponent
         {...this.props}
-        isMarkerShown
+        
         googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyAx4-7otl16MjOrkoX2K8ukjODsWbKROTs"
         loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `400px` }} />}
+        containerElement={<div style={{ height:`100%`, width:`75%` }} />}
         mapElement={<div style={{ height: `100%` }} />}
         
-      />);
+      />
+        );
     
     }
 }
